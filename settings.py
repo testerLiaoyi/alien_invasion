c6cb5845f1d86f5f713e2ed70f42d1693993ba3b
LiaoYi <woaini520>
2020-04-27 09:43:21 +0800

    Initial commit

---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings():
+    """存储《外星人入侵》的所有设置的类"""
+    def __init__(self):
+        #屏幕设置
+        self.screen_width = 1200
+        self.screen_height = 600
+        self.bg_color = (230,230,230)
