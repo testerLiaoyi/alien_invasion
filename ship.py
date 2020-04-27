241c4a0e2a60ce17d6e161c4d4d90991693a0c55
LiaoYi <woaini520>
2020-04-27 18:12:28 +0800

    添加子弹

--- ship.py
+++ ship.py
@@ -2,9 +2,10 @@ import pygame
 
 class Ship():
 
-    def __init__(self,screen):
+    def __init__(self,ai_settings,screen):
         """初始化飞船并设置其初始位置"""
         self.screen = screen
+        self.ai_settings = ai_settings
 
         #加载飞船图像并获取其外接矩形
         self.image = pygame.image.load('images/rocket.bmp')
@@ -14,6 +15,22 @@ class Ship():
         #将每艘新飞船放在屏幕底部中央
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+
+        #在飞船的属性center中存储最小值
+        self.center = float(self.rect.centerx)
+
+        #移动标识
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """根据移动标志调整飞船位置"""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        elif self.moving_left and self.rect.left > 0:
+            self.center -= self.ai_settings.ship_speed_factor
+        #根据self.center跟新rect对象
+        self.rect.centerx = self.center
 
     def blitme(self):
         """在指定位置绘制飞船"""
